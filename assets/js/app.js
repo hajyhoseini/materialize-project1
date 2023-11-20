710eb6aaa2f9f838c9897f76c4db1833dfa550d1
hajyhoseini <hajy3843@gmail.com>
2023-11-20 15:56:07 +0330

    part #29 end Index page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded',function(){
-
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems, {
+    edge : 'right',
 });
+var carouselElems = document.querySelectorAll('.carousel');
+     M.Carousel.init(carouselElems, {fullWidth:true,
+        indicators:true,
+        onCycleTo: function () {
+            let elems = document.querySelectorAll('.top_text');
+for (const item of elems){
+    item.classList.remove('isShow')
+    setTimeout(function(){
+        item.classList.add('isShow')
+    },700)
+}
+        }
+    });
+    setInterval(function () {
+        M.Carousel.getInstance(carouselElems[0]).next()
+    },5000)
+   
+        var elemsFloatingActionButton = document.querySelectorAll('.fixed-action-btn');
+         M.FloatingActionButton.init(elemsFloatingActionButton, {});
+});
+
+  
