aed055a1633d4333a99d38c549b7e160624737c9
hajyhoseini <hajy3843@gmail.com>
2023-12-22 20:40:47 +0330

    cft

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ var elems = document.querySelectorAll('.sidenav');
 var instances = M.Sidenav.init(elems, {
     edge : 'right',
 });
-var carouselElems = document.querySelectorAll('.carousel');
+var carouselElems = document.querySelectorAll('.carousel.top_carousel');
      M.Carousel.init(carouselElems, {fullWidth:true,
         indicators:true,
         onCycleTo: function () {
@@ -16,12 +16,33 @@ for (const item of elems){
 }
         }
     });
+    var galleryItems = document.querySelectorAll('.carousel.gallery_div');
+ M.Carousel.init(galleryItems, {})
+
+    var simpleItem = document.querySelectorAll('.simple_item');
+    for (const item of simpleItem){
+        item.addEventListener('click' , function () {
+            document.getElementById('gallery_section').classList.add('active');
+        })
+    }
+    document.getElementById('close_gallery_section').addEventListener('click' , function() {
+        document.getElementById('gallery_section').classList.remove('active');
+    })
     setInterval(function () {
         M.Carousel.getInstance(carouselElems[0]).next()
     },5000)
    
         var elemsFloatingActionButton = document.querySelectorAll('.fixed-action-btn');
          M.FloatingActionButton.init(elemsFloatingActionButton, {});
+         var elems = document.querySelectorAll('select');
+         M.FormSelect.init(elems, {});
+         var textAreaElem = document.querySelectorAll('.materialize-textarea');
+         M.CharacterCounter.init(textAreaElem)
+         document.getElementById('confirm_comment').addEventListener('click',function () {
+         M.toast({html: 'نظر شما با موفقیت ثبت شد!',classes:'green lighten-1 '})
+            
+         })
+       
 });
 
   
